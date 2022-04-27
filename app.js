b1a9443218972cea2e9faf70a260a8e1a1a421cd
austinbhan <austinbhan@outlook.com>
2022-04-27 16:27:27 -0700

    CSS elements showing, working on head selector

--- app.js
+++ app.js
@@ -11,15 +11,22 @@ const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
+
+let headChange = 0;
+let middleChange = 0;
+let bottomChange = 0;
+
 // set state for all of the character's catchphrases
+
+let catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
+    console.log('changing select', headDropdown.value);
     // increment the head change count state
-    
+    headChange++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url${headDropdown.value}.png`;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
